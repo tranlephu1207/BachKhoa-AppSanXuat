3f55e3579c6b62c6df07970db47c89bf1d70c850
Le Phu Tran <tranlephu1207@gmail.com>
2021-02-27 10:25:32 +0700

    upload project

---
+++ BachKhoa-AppSanXuat/LoginScreen.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+
+namespace BachKhoa_AppSanXuat
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    /// 
+ 
+    public partial class LoginScreen : Window
+    {
+
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        OracleConnection con = new OracleConnection();
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            string conString = $"User Id={txtuserName.Text}; Password={txtPassword.Text}; Data Source=bachkhoa-datastructurecourse.ctdrbnxbnuv7.us-east-2.rds.amazonaws.com:1521/XEPDB1; Pooling = false;";
+            Console.WriteLine(conString);
+            con.ConnectionString = conString;
+            try
+            {
+                if (con.State == System.Data.ConnectionState.Closed)
+                    con.Open();
+                if (con.State == ConnectionState.Open)
+                {
+                    App.Current.Properties["conString"] = conString;
+                    App.Current.Properties["con"] = con;
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    Close();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
---
+++ BachKhoa-AppSanXuat/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
+namespace BachKhoa_AppSanXuat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Console.WriteLine(App.Current.Properties["conString"]);
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OracleConnection con = App.Current.Properties["con"] as OracleConnection;
+            Console.WriteLine(con.State);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            OracleCommand cmd = con.CreateCommand();
+            // cmd.CommandText = "SELECT * FROM USER_TAB_PRIVS where privilege = 'SELECT'";
+            cmd.CommandText = "SELECT * FROM sysdba.NguyenVatLieu";
+
+            OracleDataReader reader = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            OracleDataAdapter oda = new OracleDataAdapter(cmd);
+            oda.Fill(dt);
+            dataGridview1.ItemsSource = dt.DefaultView;
+            con.Close();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
